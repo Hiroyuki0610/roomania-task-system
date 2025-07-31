ee1f0772b598341e1306375f7f6938ce6453073b
MapleHeart <terabayashi.h@gmail.com>
2025-07-31 13:01:48 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,11 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-body {
-  margin: 0;
-  font-family: 'M PLUS Rounded 1c', 'Hiragino Sans', 'Hiragino Kaku Gothic ProN', 'Yu Gothic', 'Meiryo', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: #f9fafb;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-/* PWA用のアニメーション */
-@keyframes slide-in-from-top {
-  from {
-    opacity: 0;
-    transform: translateY(-10px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-@keyframes fade-in {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-.animate-in {
-  animation-fill-mode: both;
-}
-
-.slide-in-from-top {
-  animation-name: slide-in-from-top;
-}
-
-.fade-in {
-  animation-name: fade-in;
-}
-
-.duration-200 {
-  animation-duration: 200ms;
-}
-
-.duration-500 {
-  animation-duration: 500ms;
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
